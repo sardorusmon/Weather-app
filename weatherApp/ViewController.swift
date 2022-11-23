fddbb23a3168a7f39fa8865e2f65089484690067
SardorUsmon <sardorusmon111@gmail.com>
2022-11-24 00:40:30 +0500

    Initial Commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Sardor Panjiyev on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
